250e8e6c2ef5e47180aebbab56bba2b80f34f866
Lukasz Kurek <lukascoorek@gmail.com>
2019-02-05 13:49:37 +0100

    added link to single post

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import About from "./About";
 import Contact from "./Contact";
 import Home from "./Home";
 import Navbar from "./Navbar";
+import Post from "./Post";
 
 class App extends Component {
   render() {
@@ -13,9 +14,12 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <Navbar />
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/:postId" component={Post} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
